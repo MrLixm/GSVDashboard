eac3f4ee70815749ce2de6a1b3619eb546bbdfb5
Liam Collod <lcollod@gmail.com>
2022-01-28 17:20:06 +0100

    gsvdb: GSV: wip refactoring for new features

--- src/supertools/GSVDashboard/v1/GSV.py
+++ src/supertools/GSVDashboard/v1/GSV.py
@@ -14,38 +14,12 @@ except ImportError:
 
 import NodegraphAPI
 
+from . import c
+
 
 __all__ = ["GSVNode", "GSVScene", "GSVLocal"]
 
-
-"""____________________________________________________________________________
-
-    SETUP
-
-"""
-
-
-def setup_logging(level):
-    logger = logging.getLogger("FindGSV")
-    logger.setLevel(level)
-
-    if not logger.handlers:
-        # create a file handler
-        handler = logging.StreamHandler(stream=sys.stdout)
-        handler.setLevel(logging.DEBUG)
-        # create a logging format
-        formatter = logging.Formatter(
-            '%(asctime)s - [%(levelname)7s] %(name)38s // %(message)s',
-            datefmt='%H:%M:%S'
-        )
-        handler.setFormatter(formatter)
-        # add the file handler to the logger
-        logger.addHandler(handler)
-
-    return logger
-
-
-logger = setup_logging(logging.INFO)
+logger = logging.getLogger("{}.Node".format(c.name))
 
 """ config_dict(dict)
 Configure how the script behave
@@ -74,24 +48,24 @@ CONFIG = {
     "excluded": ["gafferState"],
     "nodes": {
         "VariableSwitch": {
+            "action": "getter",
             "name": "variableName",
             "values": "patterns"
         },
         "VariableEnabledGroup": {
+            "action": "getter",
             "name": "variableName",
             "values": "pattern"
+        },
+        "VariableSet": {
+            "action": "setter",
+            "name": "variableName",
+            "values": "variableValue"
         }
-
     }
 }
 
 TIME = NodegraphAPI.GetCurrentTime()
-
-"""____________________________________________________________________________
-
-    API
-
-"""
 
 
 class GSVNode(object):
@@ -100,21 +74,34 @@ class GSVNode(object):
 
     Args:
         node(NodegraphAPI.Node):
+
+    Attributes:
+        node: Katana node
+        type: Katana node type
+        action: Action performed on GSV: setter or getter
+        gsv_name: Name of the GSV
+        gsv_values: Value(s) the GSV can take
+
     """
 
-    sources = CONFIG.get("nodes", dict())
+    __sources = CONFIG.get("nodes", dict())
 
     def __init__(self, node):
 
-        self.kobj = node
+        self.node = node
         self.type = node.getType()
+        self.action = None
+        self.gsv_name = None
+        self.gsv_values = None
+
+        self.action = self.__sources[self.type]["action"]
 
         self.gsv_name = self.get_parameter(
-            param_path=self.sources[self.type]["name"]
+            param_path=self.__sources[self.type]["name"]
         )[0]
 
         self.gsv_values = self.get_parameter(
-            param_path=self.sources[self.type]["values"]
+            param_path=self.__sources[self.type]["values"]
         )
 
         logger.debug(
@@ -126,7 +113,7 @@ class GSVNode(object):
         return
 
     def __str__(self):
-        return "{}({})".format(self.kobj.getName(), self.type)
+        return "{}({})".format(self.node.getName(), self.type)
 
     def get_parameter(self, param_path):
         """
@@ -141,11 +128,11 @@ class GSVNode(object):
             TODO support case where given param has multiple nested param
         """
 
-        param = self.kobj.getParameter(param_path)
+        param = self.node.getParameter(param_path)
         if not param:
             raise ValueError(
                 "Parameter <{}> not found on node <{}>"
-                "".format(param_path, self.kobj)
+                "".format(param_path, self.node)
             )
 
         output = list()
@@ -164,13 +151,25 @@ class GSVLocal(object):
     Represent a GSV as a python object. Allow to know which node is using this
     gsv and what value it can take.
 
+    A local GSV is considered as locked when it's value is set in the Nodegraph
+
+    # TODO get the current value this GSV is potentially set to
+
     Args:
         name(str): gsv name used in the nodegraph
         scene(GSVScene): parent scene
+
+    Attributes:
+        name: Name of the local GSV
+        scene: Parent scene this GSV can found in.
+        nodes: LIst nodes that are using this GSV.
+        values: List of value the GSV can take.
+        locked: Value the GSV is currently set to or None if not set yet.
+
     """
 
-    instances = list()
-    excluded = CONFIG.get("excluded", list())
+    __instances = list()
+    __excluded = CONFIG.get("excluded", list())
 
     def __new__(cls, *args, **kwargs):
 
@@ -178,29 +177,32 @@ class GSVLocal(object):
         scene = kwargs.get("scene") or args[1]  # type: GSVScene
 
         # If the variable name is specified as excluded return None
-        if name in cls.excluded:
+        if name in cls.__excluded:
             return None
 
         # try to find if an instance of this class with the same name and same
         # parented scene already exists.
         # If yes, return it instead of creating a new one.
-        for instance in cls.instances:
+        for instance in cls.__instances:
             if instance.name == name and instance.scene == scene:
                 return instance  # type: GSVLocal
 
         new_instance = super(GSVLocal, cls).__new__(cls)
-        cls.instances.append(new_instance)
+        cls.__instances.append(new_instance)
         return new_instance
 
     def __init__(self, name, scene):
+
         self.name = name
         self.scene = scene
         self.nodes = list()  # type: List[GSVNode]
-        self.values = list()
+        self.values = list()  # type: List[str]
+        self.locked = None  # type: Optional[str]
 
-    def _build_nodes(self):
+    def __build_nodes(self):
         """
         Find all the nodes in the scene that use the current gsv name.
+        This nodes are setter and getters.
         """
 
         self.nodes = list()
@@ -214,9 +216,10 @@ class GSVLocal(object):
 
         return
 
-    def _build_values(self):
+    def __build_values(self):
         """
-
+        Get all the potential values this GSV can take by iterating through the
+        nodes using it.
         """
 
         # reset self.value first
@@ -239,8 +242,8 @@ class GSVLocal(object):
 
     def build(self):
 
-        self._build_nodes()
-        self._build_values()
+        self.__build_nodes()
+        self.__build_values()
 
         logger.debug(
             "[GSVLocal][build] Finished for name=<{}>".format(self.name)
@@ -248,6 +251,9 @@ class GSVLocal(object):
         return
 
     def todict(self):
+        """
+        Return a dictionnary representation of the class.
+        """
 
         if not self.values:
             logger.warning("[GSVLocal][todict] self.values is empty")
@@ -279,7 +285,7 @@ class GSVScene(object):
         # reset self.nodes first
         self.nodes = list()
 
-        for node_class, _ in GSVNode.sources.items():
+        for node_class, _ in GSVNode.__sources.items():
 
             nodes = NodegraphAPI.GetAllNodesByType(node_class)  # type: list
             for node in nodes:
